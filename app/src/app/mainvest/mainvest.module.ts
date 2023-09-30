30e0deeda1222aa09dab50f17bf59d5aa4ba77a4
Jaime Jimenez <jaimejimenez99@hotmail.com>
2023-09-30 15:59:26 +0200

    [FEAT] Add menu

--- app/src/app/mainvest/mainvest.module.ts
+++ app/src/app/mainvest/mainvest.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainvestRoutingModule } from './mainvest-routing.module';
 import { MainComponent } from './main/main.component';
+import { MenuComponent } from './main/menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    MainvestRoutingModule
+    MainvestRoutingModule,
+    RouterModule
   ],
-  declarations: [MainComponent],
+  declarations: [MainComponent, MenuComponent],
 })
 export class MainvestModule { }
