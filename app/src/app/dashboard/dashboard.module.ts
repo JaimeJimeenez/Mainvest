2705cd5002eaffe0c403733356f9359599b4857d
Jimmyjimenez <jaimejimenez99@hotmail.com>
2024-02-04 18:27:19 +0100

    [FEAT] Add search asset feature

--- app/src/app/dashboard/dashboard.module.ts
+++ app/src/app/dashboard/dashboard.module.ts
@@ -4,9 +4,15 @@ import { DashboardRoutingModule } from './dashoboard-routing.module';
 import { MainComponent } from './main/main.component';
 import { MenuComponent } from './main/menu/menu.component';
 import { RouterModule } from '@angular/router';
+import { SearchComponent } from '../components/search/search.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule, NgOption } from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
+    NgSelectModule,
+    SearchComponent,
     CommonModule,
     DashboardRoutingModule,
     RouterModule,
